73e386361e0a1a11344c0756ccdaf12696737d07
Francisco Costa <francosta@protonmail.com>
2020-05-17 14:54:51 -0300

    Init camera preview

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ const cvReady = new Promise(resolve => {
   window.Module = { onRuntimeInitialized: resolve };
   window.cv = require('./img-proc/opencv');
 });
+
+let stream;
+const preview = document.getElementById('preview');
+
+async function initCameraPreview() {
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({ video: {
+      facingMode: { exact: 'environment' },
+    }});
+    preview.srcObject = stream;
+    preview.play();
+  } catch(error) {
+    // TODO: Handle exceptions (NotAllowedError | NotFoundError)
+    console.error(error);
+  }
+}
+
+initCameraPreview();
