7023ab6ef64c80b4c4a5ae6de7d804bb0d234c1f
Francisco Costa <francosta@protonmail.com>
2020-05-17 15:45:52 -0300

    Capture photo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,29 @@ const cvReady = new Promise(resolve => {
 
 let stream;
 const preview = document.getElementById('preview');
+const captureBtn = document.getElementById('capture-btn');
+
+const createImageFromBlob = blob => new Promise(resolve => {
+  const image = new Image();
+  const imageUrl = URL.createObjectURL(blob);
+  image.onload = () => {
+    URL.revokeObjectURL(imageUrl);
+    resolve(image);
+  };
+  image.src = imageUrl;
+});
+
+captureBtn.addEventListener('click', async () => {
+  try {
+    preview.pause();
+    const track = stream.getVideoTracks()[0];
+    const picture = await (new ImageCapture(track)).takePhoto();
+    const image = await createImageFromBlob(picture);
+  } catch(error) {
+    // TODO: Handle exceptions
+    console.error(error);
+  }
+});
 
 async function initCameraPreview() {
   try {
