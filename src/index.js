496305988d0e11a0ab2c873bd5821de9d0f6847a
Francisco Costa <francosta@protonmail.com>
2020-05-21 21:48:43 -0300

    Add a back button and a donwload button

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const cvReady = new Promise(resolve => {
 let stream;
 const preview = document.getElementById('preview');
 const captureBtn = document.getElementById('capture-btn');
+const backBtn = document.getElementById('back-btn');
+const downloadBtn = document.getElementById('download-btn');
 const canvas = document.getElementById('canvas');
 
 const createImageFromBlob = blob => new Promise(resolve => {
@@ -32,7 +34,10 @@ captureBtn.addEventListener('click', async () => {
     const corners = findSheetCorners(source);
     const sheet = removeSheetPerspective(source, corners);
     preview.style.display = 'none';
-    canvas.style.display = 'inherit';
+    canvas.style.display = 'initial';
+    captureBtn.style.display = 'none';
+    backBtn.style.display = 'initial';
+    downloadBtn.style.display = 'initial';
     cv.imshow(canvas, sheet);
     source.delete();
     sheet.delete();
@@ -40,6 +45,22 @@ captureBtn.addEventListener('click', async () => {
     // TODO: Handle exceptions
     console.error(error);
   }
+});
+
+backBtn.addEventListener('click', () => {
+  preview.style.display = 'initial';
+  canvas.style.display = 'none';
+  captureBtn.style.display = 'initial';
+  backBtn.style.display = 'none';
+  downloadBtn.style.display = 'none';
+  preview.play();
+});
+
+downloadBtn.addEventListener('click', () => {
+  const downloadLink = document.createElement('a');
+  downloadLink.download = `${Date.now()}.png`;
+  downloadLink.href = canvas.toDataURL();
+  downloadLink.click();
 });
 
 async function initCameraPreview() {
