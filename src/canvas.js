56332809bd150f411282eb28a1e8fcc3a2b03f8a
Francisco Costa <francosta@protonmail.com>
2020-05-24 14:50:29 -0300

    Allow draging corners around the canvas

--- src/canvas.js
+++ src/canvas.js
@@ -1,8 +1,13 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+canvas.addEventListener('touchstart', detectSelectedCorner);
+canvas.addEventListener('touchmove', moveSelectedCorner);
+canvas.addEventListener('touchcancel', deselectCorner);
+canvas.addEventListener('touchend', deselectCorner);
 const cornerIndicatorRadius = 15;
+const cornerTouchableRadius = 56;
 
-let corners, normalizeToCanvas;
+let corners, cornerBeingMoved, normalizeToCanvas, normalizeCanvasTouchToImage;
 
 /**
  * Returns a function which takes a point (coordinates `x` and `y`) from the original image and returns
@@ -13,13 +18,62 @@ const getToCanvasNormalizer = (pictureWidth, pictureHeight, canvasWidth, canvasH
   y: Math.round(y * canvasHeight / pictureHeight),
 });
 
+/**
+ * Returns a function which takes a point (coordinates `x` and `y`) from the canvas and returns
+ * the corresponding point in the image (by normalizing the size difference between the canvas and the image)
+ * This function accepts two optionals parameters, `canvasOffsetX` and `canvasOffsetY`, to compensate the x-offset
+ * and/or the y-offset of the canvas if needed
+ */
+const getToImageNormalizer = (pictureWidth, pictureHeight, canvasWidth, canvasHeight, canvasOffsetX = 0, canvasOffsetY = 0) => (x, y) => ({
+  x: Math.round((x - canvasOffsetX) * pictureWidth / canvasWidth),
+  y: Math.round((y - canvasOffsetY) * pictureHeight / canvasHeight),
+});
+
+function detectSelectedCorner(event) {
+  cornerBeingMoved = null;
+  const { clientX, clientY } = event.targetTouches[0];
+  const { x: imageX, y: imageY } = normalizeCanvasTouchToImage(clientX, clientY);
+
+  corners.some(({x, y}, cornerIndex) => {
+    const distanceToCorner = Math.hypot(x - imageX, y - imageY);
+    if (distanceToCorner < cornerTouchableRadius) {
+      cornerBeingMoved = cornerIndex;
+      drawCornersInCanvas();
+      return true;
+    }
+  });
+}
+
+function moveSelectedCorner(event) {
+  if (cornerBeingMoved === null) return;
+
+  const { clientX, clientY } = event.targetTouches[0];
+  const { x: imageX, y: imageY } = normalizeCanvasTouchToImage(clientX, clientY);
+  corners[cornerBeingMoved] = {
+    x: imageX,
+    y: imageY,
+  };
+  drawCornersInCanvas();
+}
+
+function deselectCorner() {
+  cornerBeingMoved = null;
+  drawCornersInCanvas();
+}
+
 function drawCornersInCanvas() {
   if (!corners || !corners.length) {
     return;
   }
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  corners.forEach(({ x: imageX, y: imageY }) => {
+  corners.forEach(({ x: imageX, y: imageY }, index) => {
+    if (index == cornerBeingMoved) {
+      ctx.fillStyle = '#000';
+    } else {
+      ctx.fillStyle = '#1E88E5';
+    }
+
     ctx.beginPath();
     const { x: canvasX, y: canvasY } = normalizeToCanvas(imageX, imageY);
     ctx.arc(canvasX, canvasY, cornerIndicatorRadius, 0, 2 * Math.PI);
@@ -33,8 +87,8 @@ export function drawCorners(detectedCorners, pictureWidth, pictureHeight) {
   canvas.style.display = 'initial';
   canvas.width = canvas.scrollWidth;
   canvas.height = canvas.scrollHeight;
-  ctx.fillStyle = '#1E88E5';
   normalizeToCanvas = getToCanvasNormalizer(pictureWidth, pictureHeight, canvas.width, canvas.height);
+  normalizeCanvasTouchToImage = getToImageNormalizer(pictureWidth, pictureHeight, canvas.width, canvas.height, canvas.offsetLeft, canvas.offsetTop);
 
   drawCornersInCanvas();
 }
